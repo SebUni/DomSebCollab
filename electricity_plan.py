0c46a78441725fcd4c819071351780637e6ed012
SebUni <s3739258@student.rmit.edu.au>
2020-12-12 23:57:47 +0100

    Added electricity plan (tested) and electricity plan manager (untested so far)

---
+++ electricity_plan.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 12 17:01:55 2020
+
+@author: S3739258
+"""
+
+import sys
+
+class ElectricityPlan():
+    """
+    Container for data of an individual electricity plan.
+    """
+    def __init__(self, uid, is_commerical_plan, base, tariff, time_step):
+        """
+        Parameters
+        ----------
+        uid : int
+            Unique Id of the electricity plan.
+        is_commerical_plan : boolean
+            Plans can either be residential or commercial.
+        base : float
+            Monthly base rate for a plan.
+        tariff : {start_time, cost_per_kWh}
+            start_time given as int and noted in hours.
+            cost_per_kWh is given in Dolars.
+            First key requires start_time = 0.
+            For example: {0: 0.40, 8: 0.50, 24: 0.40}
+        time_step : int
+            time_step given in minutes
+        Returns
+        -------
+        None.
+
+        """
+        self.uid = uid
+        self.is_commerical_plan = bool(is_commerical_plan)
+        try:
+            self.base = base
+        except ValueError:
+            sys.exit("Base of electricity plan " + str(uid) \
+                     + " is ill-defined!")
+        # check that time_step is adequatly chosen
+        if (24*60) % time_step != 0:
+            sys.exit("time_step is not adequatly chosen!")
+        for time_slot_start in range(0, 24*60, time_step):
+            sum_tariff = 0.0
+            sum_weight = 0
+            time_slot_end = time_slot_start + time_step
+            for tariff_start in list(tariff.keys()):
+                tariff_end = 24*60 # next tariff start
+                for sub_tariff_start in list(tariff.keys()):
+                    if tariff_start < sub_tariff_start < tariff_end:
+                        tariff_end = sub_tariff_start
+        
+                weight = 0
+                if tariff_start <= time_slot_start < tariff_end < time_slot_end:
+                    weight = (tariff_end - time_slot_start) / time_step
+                if time_slot_start <= tariff_start < tariff_end <= time_slot_end:
+                    weight = (time_slot_end - time_slot_start) / time_step
+                if time_slot_start < tariff_start < time_slot_end <= tariff_end:
+                    weight = (time_slot_end - tariff_start) / time_step
+                if tariff_start <= time_slot_start < time_slot_end < tariff_end:
+                    weight = 1
+                if tariff_start < time_slot_start < time_slot_end <= tariff_end:
+                    weight = 1
+        
+                sum_tariff += weight*tariff[tariff_start]
+                sum_weight += weight
+            self.tariff[time_slot_start] = sum_tariff / sum_weight
+    
+    def cost_of_use(self, used_kWh, time_of_use):
+        """
+        Calculates the price the consumer has to pay for consumed electricity.
+
+        Parameters
+        ----------
+        used_kWh : float
+            Consumed power.
+        time_of_use : int
+            Start time of the time of use interval, given in minutes since
+            midnight.
+
+        Returns
+        -------
+        float.
+        """
+        return self.tariff[time_of_use]*used_kWh
---
+++ electricity_plan_manager.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 12 23:08:44 2020
+
+@author: S3739258
+"""
+
+import csv
+import sys
+import random
+
+from electricity_plan import ElectricityPlan
+
+class ElectricityPlanManager():
+    """
+    Loads and holds all electricity plans for easy access.
+    """
+    def __init__(self, time_step):
+        """
+        Parameters
+        ----------
+        time_step : int
+            time_step given in minutes
+
+        Returns
+        -------
+        None.
+        """
+        self.electricity_plans = dict()
+        self.commercial_plan_uids = []
+        self.residential_plans_uids = []
+        self.load_electricity_plans(time_step)
+        
+    def load_electricity_plans(self, time_step):
+        """
+        Loads all electricity plans from electricity_plans.csv.
+
+        Parameters
+        ----------
+        time_step : int
+            time_step given in minutes
+
+        Returns
+        -------
+        None.
+        """
+        self.electricity_plans = dict()
+
+        with open('electricity_plans.csv', newline='') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for row in spamreader:
+                uid_str, is_commercial_plan, base = row[0], row[1], row[2]
+                tariff_str = row[3]
+                # here only uid is checked for key of dict
+                # data validity of rest is checked in ChargerModel-constructor
+                try:
+                    uid = int(uid_str)
+                except ValueError:
+                    sys.exit("Uid of charger model is ill defined!")
+                # 
+                tariff = self.extract_tariffs_from_string(tariff_str)
+                ep = ElectricityPlan(uid, is_commercial_plan, base, tariff)
+                self.electricity_plans[uid] = ep
+                if ep.is_commercial_plan:
+                    self.commercial_plan_uids.append(ep.uid)
+                else:
+                    self.residential_plans_uids.append(ep.uid)
+    
+    def extract_tariffs_from_string(self, input_str):
+        """
+        Breaks down the tariff string imported from a electricity_plans.csv
+        
+        String must follow following syntax:
+        tariff_start_time1:tariff_price1;tariff_start_time2:tariff_price2;\
+        ...;tariff_start_timeN:tariff_priceN
+
+        Caution: There is no trailing semni-collon!
+
+        Parameters
+        ----------
+        input_str : string
+            Must follow syntax as descirbed above.
+
+        Returns
+        -------
+        tariffs : dict
+            Returns tariff structure required for ElectricityPlan class.
+        """
+        tariffs = dict()
+        tariffs_str = input_str.split(';')
+        err_msg = "tariff_string '" + input_str + "' is ill-defined. \n\n"
+        err_msg += "Use syntax 'start_time1:price1;...;start_timeN:priceN'!"
+        for tariff_str in tariffs_str:
+            tariff_str.split(':')
+            try:
+                tariff_start_time = int(tariff_str[0])
+            except ValueError:
+                sys.exit(err_msg)
+            try:
+                tariff_price = float(tariff_str[1])
+            except:
+                sys.exit(err_msg)
+            tariffs[tariff_start_time] = tariff_price
+        return tariffs
+    
+    def pick_plan_at_random(self, is_commercial_plan):
+        """
+        Returns an ElectricityPlan drawn at random. It can be determined if it
+        should be a residential or a commercial plan.
+
+        Parameters
+        ----------
+        is_commercial_plan : boolean
+            Allows for choice between commercial and residential plans.
+
+        Returns
+        -------
+        ElectricityPlan
+            As explained above.
+
+        """
+        uid = 0
+        if is_commercial_plan:
+            uid = random.choice(self.commercial_plan_uids)
+        else:
+            uid = random.choice(self.residential_plans_uids)
+        return self.electricity_plans[uid]
