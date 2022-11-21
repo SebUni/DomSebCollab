b5bc09a13cf371e3256a71f1529b9c497fb0adcc
SebUni <66459073+SebUni@users.noreply.github.com>
2022-11-21 14:47:02 +0100

    6k_2d_mdl9_pw04-32_epc07-73

---
+++ results/6k_2d_mdl9_pw04-32_epc19-25/clock.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 25 23:47:02 2020
+
+@author: S3739258
+"""
+
+import sys
+import gc
+
+class Clock():
+    """
+    This object is used to have syncronised time in all other objects.
+    """
+    def __init__(self, parameters):
+        """
+        Well it creates the clock object.
+
+        Parameters
+        ----------
+        time_step: int
+            Length of each time step in minutes.
+            
+        time_step_limit: int
+            Amount of time steps executed before simulation termination.
+            
+        season: int
+            See season_name dict for definition.
+        Returns
+        -------
+        None.
+
+        """
+        self.elapsed_time = 0
+        self.cur_time_step = 0;
+        self.pre_heat_steps = parameters.get("pre_heat_steps", "int")
+        self.is_pre_heated = False
+        self.season = parameters.get("season","int")
+        self.time_step = parameters.get("time_step","int")
+        self.time_step_limit = parameters.get("time_step_limit", "int")
+        self.forecast_horizon = parameters.get("forecast_horizon", "int")
+        
+        # This boolean indicates if step() has ever been called. If not the
+        # clock does not step forward. This is done so that the first round can
+        # be initalised with clock.step() to receive the right time. In turn,
+        # this is done so that by the end of each step() the time safed in
+        # clock is still the correct time of the current round.
+        self.first_step_call = True
+        
+        self.time_of_day = self.calc_time_of_day()
+        self.time_of_week = self.calc_time_of_week()
+        self.season_names = {0 : "spring",
+                             1 : "summer",
+                             2 : "autumn",
+                             3 : "winter"}
+        season_start_in_min = {0: 60*24*(31+28+31+30+31+30+31+31+30),
+                               1: 60*24*(0),
+                               2: 60*24*(31+28+31),
+                               3: 60*24*(31+28+31+30+31+30)}
+        next_season = self.season + 1 if self.season != 3 else 0
+        self.season_in_min = {"Start":season_start_in_min[self.season],
+                              "End":season_start_in_min[next_season]}
+        if next_season == 1:
+            self.season_in_min["End"] \
+                = 60*24*(31+28+31+30+31+30+31+31+30+31+30+31)
+        if not (0 <= self.season <= 3):
+            sys.exit("Season ill defined. Must be in [0,3]")
+        
+    def calc_time_of_day(self):
+        """ Calculates how many minutes have elapsed since the most recent
+        mid-night. """
+        return self.elapsed_time % (60*24)
+    
+    def calc_time_of_week(self):
+        """ Calculates how many minutes have elapsed since the most recent
+        Sunday to Monday mid-night. """
+        return self.elapsed_time % (60*24*7)
+        
+    def step(self):
+        """
+        Elapses time by the defined time_step.
+
+        Returns
+        -------
+        None.
+
+        """
+        self.cur_time_step += 1
+        if self.cur_time_step >= self.pre_heat_steps:
+            self.is_pre_heated = True
+        if not self.first_step_call:
+            self.elapsed_time += self.time_step
+            self.time_of_day = self.calc_time_of_day()
+            self.time_of_week = self.calc_time_of_week()
+        else:
+            self.first_step_call = False
+            
+    def clear(self):
+        attrs = ["elapsed_time", "cur_time_step", "pre_heat_steps",
+                 "is_pre_heated", "season", "time_step", "time_step_limit", 
+                 "forecast_horizon", "first_step_call", "time_of_day", 
+                 "time_of_week", "season_names", "season_in_min"]
+        for attr in attrs:
+            if hasattr(self, attr):
+                if hasattr(getattr(self, attr), "clear"):
+                    getattr(self, attr).clear()
+                delattr(self, attr)
+        gc.collect()
---
+++ results/6k_2d_mdl9_pw04-32_epc55-61/whereabouts_manager.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  2 10:57:16 2020
+
+@author: S3739258
+"""
+
+import whereabouts
+
+class WhereaboutsManager():
+    """
+    Managaes the individual Whereabouts.
+    
+    This class is implement to simplify the interaction between all
+    Whereabouts-classes.
+    """
+    def __init__(self, location_road_manager, clock, calendar_planner):
+        """
+        Initialises the whereabouts manager, which keeps track of agents
+        journeys.
+
+        Parameters
+        ----------
+        location_road_manager : LocationRoadManager
+            Handle to the location road manager.
+        clock: Clock
+            The instance of the clock module that provides information on time.
+        Returns
+        -------
+        None.
+        """
+        self.lrm = location_road_manager
+        self.cp = calendar_planner
+        self.whereabouts = dict()
+        self.time_step = clock.time_step
+        
+    def track_new_agent(self, agent_uid, cur_activity, cur_location):
+        """ Adds tracking of a new agent to the manager. """
+        self.whereabouts[agent_uid] \
+            = whereabouts.Whereabouts(agent_uid, cur_activity, cur_location,
+                                      self.lrm, self.cp)
+        return self.whereabouts[agent_uid]
+    
+    def count_agents_on_edges(self):
+        """ Counts how many agents are on each edge.
+        
+        Needed to estimate congestions and calculate agents velocity."""
+        agents_on_edge = dict()
+        for edge in list(self.lrm.traffic_network.edges):
+            count = 0
+            for whereabouts_it in self.whereabouts.values():
+                if whereabouts_it.cur_edge == edge:
+                    count += 1
+            agents_on_edge[edge] = count
+        
+        return agents_on_edge
+    
+    def prepare_movement(self):
+        """ Determines all parameters, especially the velocity, required by
+        Agents to calculate their next movement. """
+        agents_on_edges = self.count_agents_on_edges()
+        self.lrm.determine_current_velocity_on_edges(agents_on_edges)
